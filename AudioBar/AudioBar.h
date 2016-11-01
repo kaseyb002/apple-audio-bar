671577be6abf6fac173d918546c2734feb0ad4fc
Rudolf Adamkovič <salutis@me.com>
2016-11-01 12:27:19 +0100

    Remove Xcode template garbage

--- AudioBar/AudioBar.h
+++ AudioBar/AudioBar.h
@@ -1,19 +1,4 @@
-//
-//  AudioBar.h
-//  AudioBar
-//
-//  Created by Rudolf Adamkovic on 1.11.2016.
-//  Copyright © 2016 TBD. All rights reserved.
-//
+#import <Foundation/Foundation.h>
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for AudioBar.
 FOUNDATION_EXPORT double AudioBarVersionNumber;
-
-//! Project version string for AudioBar.
 FOUNDATION_EXPORT const unsigned char AudioBarVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AudioBar/PublicHeader.h>
-
-
