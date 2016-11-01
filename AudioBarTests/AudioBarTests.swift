9566728916a02869eb4c2ead1b490ed04e178dd5
Rudolf Adamkovič <salutis@me.com>
2016-11-01 12:26:47 +0100

    Add empty AudioBar class

--- AudioBarTests/AudioBarTests.swift
+++ AudioBarTests/AudioBarTests.swift
@@ -26,11 +26,9 @@ class AudioBarTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAudioBarType() {
+        let audioBar = AudioBar()
+        XCTAssert(audioBar as Any is UIView)
     }
     
 }
