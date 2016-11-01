c94f4993651caec18063b94a94b837a42688d10d
Rudolf Adamkovič <salutis@me.com>
2016-11-01 12:21:49 +0100

    Initial commit

---
+++ AudioBar/AudioBar.h
@@ -0,0 +1,19 @@
+//
+//  AudioBar.h
+//  AudioBar
+//
+//  Created by Rudolf Adamkovic on 1.11.2016.
+//  Copyright © 2016 TBD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudioBar.
+FOUNDATION_EXPORT double AudioBarVersionNumber;
+
+//! Project version string for AudioBar.
+FOUNDATION_EXPORT const unsigned char AudioBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioBar/PublicHeader.h>
+
+
