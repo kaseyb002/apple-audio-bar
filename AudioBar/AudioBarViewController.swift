a7a31978ce164a0a9d87149aa0171864dc65e4ad
Rudolf Adamkovič <salutis@me.com>
2017-03-07 19:00:16 +0100

    Extract CMTime+TimeInterval

--- AudioBar/AudioBarViewController.swift
+++ AudioBar/AudioBarViewController.swift
@@ -179,15 +179,3 @@ public final class AudioBarViewController: UIViewController, StoreDelegate {
     }
 
 }
-
-extension CMTime {
-
-    init(timeInterval: TimeInterval) {
-        self = CMTime(seconds: timeInterval, preferredTimescale: 44100)
-    }
-
-    var timeInterval: TimeInterval {
-        return CMTimeGetSeconds(self)
-    }
-
-}
